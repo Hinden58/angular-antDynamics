fa39e69d194bd58bd8100a0610693d54945563ca
Hinden58 <nathan.vanhille@gmail.com>
2020-05-23 14:18:14 +0200

    1 file modified: Updated anthill, breaking up storage array into simple fields

--- src/app/classes/anthill.ts
+++ src/app/classes/anthill.ts
@@ -5,13 +5,14 @@ export class Anthill {
   _name : string;
   _entrance : Element[];
   _colony : Ant[];
-  _storage : number[];
+  _storage_food : number;
+  _storage_water : number
 
   constructor(name:string, entrance:Element[], colony:Ant[], food_storage:number, water_storage:number) {
     this._name = name;
     this._entrance = entrance;
     this._colony = colony;
-    this._storage[0] = food_storage;
-    this._storage[1] = water_storage;
+    this._storage_food = food_storage;
+    this._storage_water = water_storage;
   }
 }
